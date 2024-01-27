6330bdbaef8475fece1b3d3f092b57bf2377ee24
Jurģis Vilnis <graunder@gmail.com>
2024-01-27 04:52:13 +0200

    target, right stick controls

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
-var moveSpeed = 500
+var moveSpeed = 100
+
+@onready var target = $Target
 
 func _physics_process(delta):
 	
 	var direction_x = Input.get_axis("left", "right")
 	var direction_y = Input.get_axis("forward", "backward")
+	
 	velocity.x = 0
 	velocity.y = 0
 	
@@ -13,5 +16,6 @@ func _physics_process(delta):
 		velocity.x = direction_x * moveSpeed
 	if direction_y:
 		velocity.y = direction_y * moveSpeed
+		
 	
 	move_and_slide()
---
+++ Assets/Scripts/Target.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var speed = 100
+
+func _process(delta):
+	var direction_look_x = Input.get_axis("look_left", "look_right")
+	var direction_look_y = Input.get_axis("look_up", "look_down")
+	
+	if direction_look_x:
+		position.x = direction_look_x * speed
+	if direction_look_y:
+		position.y = direction_look_y * speed
