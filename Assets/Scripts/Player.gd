ab51ce805c3f70d4c6deb8f3d08c7a292dd6b278
Jurģis Vilnis <graunder@gmail.com>
2024-01-28 04:14:31 +0200

    player rotation

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -4,9 +4,14 @@ var moveSpeed = 500
 var bulletSpeed = 2000
 var bullet = preload("res://Assets/Scenes/Bullet.tscn")
 
-@onready var target = $Target
+@onready var target = $"../Target"
 
-func _physics_process(delta):
+func _process(delta):
+	
+	var joystick_vector = Vector2(Input.get_joy_axis(0, JOY_AXIS_RIGHT_X), Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y))
+	var joystick_angle = atan2(joystick_vector.y, joystick_vector.x)
+	print(joystick_angle)
+	rotation = joystick_angle
 	
 	var direction_x = Input.get_axis("left", "right")
 	var direction_y = Input.get_axis("forward", "backward")
@@ -28,5 +33,5 @@ func fire():
 	var bullet_instance = bullet.instantiate()
 	bullet_instance.position = get_global_position()
 	bullet_instance.rotation_degrees = global_position.direction_to(target.position).angle()
-	bullet_instance.apply_impulse(Vector2(), Vector2(bulletSpeed, 0))
+	bullet_instance.apply_impulse(Vector2(), Vector2(bulletSpeed, 0).rotated(global_rotation))
 	get_tree().get_root().call_deferred("add_child", bullet_instance)
--- Assets/Scripts/Target.gd
+++ Assets/Scripts/Target.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var speed = 100
 
-func _process(delta):
+func _process(delta):	
 	var direction_look_x = Input.get_axis("look_left", "look_right")
 	var direction_look_y = Input.get_axis("look_up", "look_down")
 	
