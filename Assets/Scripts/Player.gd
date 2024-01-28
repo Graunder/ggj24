37a3243c89a2ea28fd8ed5e7aeee013924fcf250
Jurģis Vilnis <graunder@gmail.com>
2024-01-28 03:23:51 +0200

    reset point

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 var moveSpeed = 500
+var bulletSpeed = 2000
+var bullet = preload("res://Assets/Scenes/Bullet.tscn")
 
 @onready var target = $Target
 
@@ -17,5 +19,14 @@ func _physics_process(delta):
 	if direction_y:
 		velocity.y = direction_y * moveSpeed
 		
-	
+	if (Input.is_action_just_pressed("shoot")):
+		fire()
+		
 	move_and_slide()
+
+func fire():
+	var bullet_instance = bullet.instantiate()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = global_position.direction_to(target.position).angle()
+	bullet_instance.apply_impulse(Vector2(), Vector2(bulletSpeed, 0))
+	get_tree().get_root().call_deferred("add_child", bullet_instance)
